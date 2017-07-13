7c598b95c5b46408c122fa345671872e48e1618a
root <ntbandara3@hotmail.com>
2017-07-13 15:35:52 +0530

    firebase configuration

--- app.js
+++ app.js
@@ -2,9 +2,24 @@
  * Created by root on 6/28/17.
  */
 angular.module('ttt',[
+    //firebase
+    'firebase',
     'ttt.routes',
     //controllers
     'ttt.homeController',
     'ttt.gameController'
     //services
 ])
+
+    //firebase configuration
+.config([function(){
+    var config = {
+        apiKey: "AIzaSyBBSEgC5EGzXTOTJMY1wZZguiYCPiQapKg",
+        authDomain: "tic-tac-toe-142a6.firebaseapp.com",
+        databaseURL: "https://tic-tac-toe-142a6.firebaseio.com",
+        projectId: "tic-tac-toe-142a6",
+        storageBucket: "tic-tac-toe-142a6.appspot.com",
+        messagingSenderId: "249878909577"
+    };
+    firebase.initializeApp(config);
+}])
